9e329dd0cffb809c566a3ae348d6a659c3a4c716
contato.gprado@gmail.com <contato.gprado@gmail.com>
2024-07-05 22:18:01 -0300

    clear the form's inputs after includes a expense

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -96,6 +96,9 @@ function expenseAdd(newExpense) {
     // Adiciona o item à lista.
     expenseList.append(expenseItem)
 
+    // Limpa o formulário para adicionar um novo item.
+    formClear()
+
     // Atualiza os totais.
     updateTotals()
 
@@ -168,3 +171,13 @@ expenseList.addEventListener('click', function (event) {
   // Atualiza os totais.
   updateTotals()
 })
+
+function formClear() {
+  // Limpa os inputs.
+  expense.value = ''
+  category.value = ''
+  amount.value = ''
+
+  // Coloca o foco no input de amount.
+  expense.focus()
+}
